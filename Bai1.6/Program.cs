124503ab168d7e43008dceef4a01b4999d48041e
Tri Huynh <22521519@gm.uit.edu.vn>
2023-10-07 11:34:37 +0700

    '1.6'

---
+++ Bai1.6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+
+class Mamal
+{
+    private string characteristics;
+    public string Characteristics
+    {
+        get { return characteristics; } 
+        set { characteristics = value; }
+    }
+}
+class Whale : Mamal
+{
+    public Whale()
+    {
+        Console.WriteLine("This is a whale");
+    }
+}
+class Human : Mamal, IAbility
+{
+    public Human()
+    {
+        Console.WriteLine("This is a human");
+    }
+    public void thinking_behaviour()
+    {
+        Console.WriteLine("I'm thinking");
+    }
+    public void intelligent_behaviour()
+    {
+        Console.WriteLine("I'm doing intelligent things");
+    }
+}
+
+interface IThinking
+{
+    void thinking_behaviour();  
+}
+interface IIntelligent
+{
+    void intelligent_behaviour();
+}
+
+interface IAbility : IThinking, IIntelligent
+{
+
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Whale whale = new Whale();
+        Human human = new Human();
+        human.thinking_behaviour();
+        human.intelligent_behaviour();
+    }
+}

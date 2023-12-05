a7fcaff3d1b9e67af192d648d26bfe5ef0a8af0e
Bui Thai Hoang <127731981+BTH260704@users.noreply.github.com>
2023-12-06 03:44:23 +0700

    btnNext, btnPre, Play, UI

---
+++ AppMusic/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMusic
+{
+    public class DataProvider
+    {
+        public static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+        public MUSICAPPEntities DB { get; set; }
+        private DataProvider()
+        {
+            DB = new MUSICAPPEntities();
+        }
+    }
+}
